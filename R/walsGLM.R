a23e85cac52e480b40090b126e3330aae2fd6718
kevhuy <kevin.huynh@unibas.ch>
2023-09-12 11:01:54 +0200

    Changed order of walsGLM methods and updated docs with more arguments, returns and examples.

--- R/walsGLM.R
+++ R/walsGLM.R
@@ -11,6 +11,225 @@
 #' @export
 walsGLM <- function(x, ...) UseMethod("walsGLM", x)
 
+#' \code{walsGLM.formula} uses formulas to specify the design matrix.
+#' @rdname walsGLM
+#'
+#' @param formula an object of class "\link{Formula}"
+#' (or one that can be coerced to that class):
+#' a symbolic description of the model to be fitted.
+#' The details of model specification are given under ‘Details’.
+#' @param family Object of class \code{familyWALS}.
+#' @param data an optional data frame, list or environment
+#' (or object coercible by as.data.frame to a data frame) containing the
+#' variables in the model. If not found in data, the variables are taken from
+#' environment(formula), typically the environment from which walsNB is called.
+#' @param subset an optional vector specifying a subset of observations to be
+#' used in the fitting process.
+#' @param na.action **not implemented yet.**
+#' @param weights **not implemented yet.**
+#' @param offset **not implemented yet.**
+#' @param prior Object of class \code{familyPrior}. For example \link[WALS]{weibull}
+#' or \link[WALS]{laplace}.
+#' @param controlGLMfit Controls estimation of starting values for one-step ML,
+#' passed to \link[stats]{glm.fit}. See also \link[stats]{glm.control}.
+#' @param model if \code{TRUE} (default), then the model.frame is stored in
+#' the return.
+#' @param keepY if \code{TRUE} (default), then the response is stored in
+#' the return.
+#' @param keepX if \code{TRUE}, then the model matrix is stored in the return.
+#' the return.
+#' @param iterate if TRUE then the WALS algorithm is iterated using the previous
+#' estimates as starting values
+#' @param tol Only used if iterate = TRUE and nIt = NULL. If the Euclidean distance
+#' between the previous beta and current beta falls below tol, then the algorithm stops.
+#' @param maxIt Only used it iterate = TRUE and nIt = NULL. Aborts iterative fitting
+#' when number of iterations exceed maxIt
+#' @param nIt Only used if iterate = TRUE. If this is specified, then tol is ignored
+#' and the algorithm iterates nIt times. This option should not be used unless
+#' the user has a specific reason to run the algorithm nIt times, e.g. for
+#' replication purposes.
+#' @param verbose If verbose = TRUE, then it prints the iteration process
+#' (only relevant if iterate = TRUE).
+#' @param ... Arguments for workhorse \link[WALS]{walsGLMfit}.
+#'
+#' @details
+#' Formulas should always contain two parts, i.e. they should be of the form
+#' "y ~ X11 + X12 | X21 + X22", where the variables before "|" are the focus
+#' regressors (includes a constant by default) and the ones after "|" are the
+#' auxiliary regressors.
+#'
+#' **WARNING:** Interactions in formula do work work properly yet.
+#' It is recommended to manually create the interactions beforehand and then
+#' to insert them as 'linear terms' in the formula.
+#'
+#' @returns For \code{walsGLM.formula}, it returns an object of class
+#' \code{walsGLM} which inherits from \code{walsGLM}. It contains
+#' all elements returned from \link[WALS]{walsGLMfitIterate} and additionally
+#' \item{cl}{Call of the function.}
+#' \item{formula}{\code{formula} used.}
+#' \item{terms}{List containing the model terms of the focus and auxiliary
+#' regressors separately, as well as for the full model.}
+#' \item{levels}{List containing the levels of the focus and auxiliary
+#' regressors separately, as well as for the full model.}
+#' \item{contrasts}{List containing the contrasts of the design matrices of
+#' focus and auxiliary regressors.}
+#' \item{model}{If \code{model = TRUE}, contains the model frame.}
+#'
+#' See returns of \link[WALS]{walsGLMfit} and \link[WALS]{walsGLMfitIterate}
+#' for more details.
+#'
+#' @examples
+#' data("HMDA", package = "AER")
+#' fitBinomial <- walsGLM(deny ~ pirat + hirat + lvrat + chist + mhist + phist |
+#'                        selfemp + afam, data = HMDA, family = binomialWALS(),
+#'                        prior = weibull())
+#' summary(fitBinomial)
+#'
+#' data("NMES1988", package = "AER")
+#' fitPoisson <- walsGLM(emergency ~ health + chronic + age + gender |
+#'                       I((age^2)/10) + married + region, data = NMES1988,
+#'                       family = poissonWALS(), prior = laplace())
+#' summary(fitPoisson)
+#'
+#' @export
+walsGLM.formula <- function(formula, family, data, subset = NULL,
+                            na.action = NULL, weights = NULL, offset = NULL,
+                            prior = weibull(), controlGLMfit = list(),
+                            model = TRUE, keepY = TRUE, keepX = FALSE,
+                            iterate = FALSE, tol = 1e-6, maxIt = 10000, nIt = NULL,
+                            verbose = FALSE, ...) {
+
+  ## call
+  cl <- match.call()
+  if (missing(data)) data <- environment(formula)
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "na.action", "weights", "offset"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+
+  ## formula
+  oformula <- as.formula(formula)
+  formula <- Formula::as.Formula(formula)
+
+  # remove intercept in 2nd part by default
+  # if 2nd part already includes "-1" it will remain unchanged
+  # formula <- update(formula, . ~ . | . + -1)
+
+  if (length(formula)[2L] < 2L) {
+    # TODO: Implement what happens when only one part is specified in formula
+    # Either interpret all as focus regressors (same as ML estimator basically)
+    # or include all as auxiliary regressors and keep only constant as
+    # focus regressor.
+
+    stop("One part formula not implemented yet")
+    # formula <- as.Formula(formula(formula), ~ 1)
+    # simpleFormula <- TRUE
+  } else {
+    if (length(formula)[2L] > 2L) {
+      formula <- Formula::Formula(formula(formula, rhs = 1:2))
+      warning("formula must not have more than two RHS parts")
+    }
+    simpleFormula <- FALSE
+  }
+  mf$formula <- formula
+
+  ## evaluate model.frame
+  mf[[1L]] <- as.name("model.frame")
+  mf <- eval(mf, parent.frame())
+
+  ## extract terms, model matrix, response
+  mm <- extractModel(formula, mf, data)
+
+  # extract objects from mm
+  Y <- mm$Y; X1 <- mm$X1; X2 <- mm$X2; mt <- mm$mt; mtX1 <- mm$mtX1; mtX2 <- mm$mtX2
+  cont <- mm$cont
+  n <- length(Y)
+
+  ## weights (not used yet)
+  weights <- processWeights(weights, mf, n)
+
+  ## offsets (not used yet)
+  offset <- getOffset(formula, mf, cl, n)
+
+  ## Fit model
+  out <- walsGLMfitIterate(Y, X1, X2, family, na.action, weights, offset, prior,
+                           controlGLMfit, keepY, keepX, iterate, tol, maxIt,
+                           nIt, verbose, ...)
+
+  # add more elements
+  out$call <- cl
+  out$formula <- oformula
+  out$terms <- list(focus = mtX1, aux = mtX2, full = mt)
+  out$levels <- list(focus = .getXlevels(mtX1, mf), aux = .getXlevels(mtX2, mf),
+                     full = .getXlevels(mt, mf))
+  out$contrasts <- cont
+  if (model) out$model <- mf
+
+
+  class(out) <- c("walsGLM", "wals")
+  return(out)
+}
+
+#' \code{walsGLM.matrix()} uses prespecified design matrices x (focus) and
+#' x2 (auxiliary) and response vector y.
+#' @rdname walsGLM
+#'
+#' @param x Design matrix for focus regressors. Usually includes a constant
+#' (column full of 1's) and can be generated using model.matrix().
+#' @param x2 Design matrix for auxiliary regressors. Usually does not include
+#' a constant column and can also be generated using model.matrix().
+#' @param y Count response as vector
+#'
+#' @returns For \code{walsGLM.matrix}, it returns an object of class
+#' \code{walsGLMmatrix}, which inherits from \code{walsGLM}, \code{walsMatrix}
+#' and \code{wals}. It contains all elements returned from
+#' \link[WALS]{walsGLMfitIterate} and additionally the call in \code{cl}.
+#'
+#' @examples
+#' ## Example for walsGLM.matrix()
+#' data("HMDA", package = "AER")
+#' X <- model.matrix(deny ~ pirat + hirat + lvrat + chist + mhist + phist + selfemp + afam,
+#'                   data = HMDA)
+#' X1 <- X[,c("(Intercept)", "pirat", "hirat", "lvrat", "chist2", "chist3",
+#'         "chist4", "chist5", "chist6", "mhist2", "mhist3", "mhist4", "phistyes")]
+#' X2 <- X[,c("selfempyes", "afamyes")]
+#' y <- HMDA$deny
+#' fit <- walsGLM(X1, X2, y, family = binomialWALS(), prior = weibull())
+#' summary(fit)
+#'
+#' @export
+walsGLM.matrix <- function(x, x2, y, family, subset = NULL, na.action = NULL,
+                           weights = NULL, offset = NULL,
+                           prior = weibull(), controlGLMfit = list(),
+                           keepY = TRUE, keepX = FALSE,
+                           iterate = FALSE, tol = 1e-6, maxIt = 10000, nIt = NULL,
+                           verbose = FALSE, ...) {
+  cl <- match.call()
+  X1 <- x
+  X2 <- x2
+  if (!is.null(subset)) {
+    X1 <- X1[subset,]; X2 <- X2[subset,]; y <- y[subset]
+  }
+
+  out <- walsGLMfitIterate(y, X1, X2, family, na.action, weights, offset, prior,
+                           controlGLMfit, keepY, keepX, iterate, tol, maxIt,
+                           nIt, verbose, ...)
+
+  out$call <- cl
+
+  class(out) <- c("walsGLMmatrix", "walsGLM", "walsMatrix", "wals")
+  return(out)
+}
+
+#' @rdname walsGLM
+#' @export
+walsGLM.default <- function(x, ...) {
+  # inspired by glmboost.default in mboost.
+  if (extends(class(x), "matrix")) {
+    return(walsGLM.matrix(x, ...))
+  }
+  stop("No method for objects of class ", sQuote(class(x)), " implemented.")
+}
 
 #' Fitter function for Weighted Average Least Squares estimation of GLMs
 #'
@@ -216,175 +435,6 @@ walsGLMfitIterate <- function(y, X1, X2, family, na.action = NULL,
   out$deviance <- sum(family$dev.resids(out$y, mu, wt))
   out$residuals <- out$y - mu
 
-  return(out)
-}
-
-
-#' @rdname walsGLM
-#' @export
-walsGLM.default <- function(x, ...) {
-  # inspired by glmboost.default in mboost.
-  if (extends(class(x), "matrix")) {
-    return(walsGLM.matrix(x, ...))
-  }
-  stop("No method for objects of class ", sQuote(class(x)), " implemented.")
-}
-
-#' @rdname walsGLM
-#' @export
-walsGLM.matrix <- function(x, x2, y, family, subset = NULL, na.action = NULL,
-                           weights = NULL, offset = NULL,
-                           prior = weibull(), controlGLMfit = list(),
-                           keepY = TRUE, keepX = FALSE,
-                           iterate = FALSE, tol = 1e-6, maxIt = 10000, nIt = NULL,
-                           verbose = FALSE, ...) {
-  cl <- match.call()
-  X1 <- x
-  X2 <- x2
-  if (!is.null(subset)) {
-    X1 <- X1[subset,]; X2 <- X2[subset,]; y <- y[subset]
-  }
-
-  out <- walsGLMfitIterate(y, X1, X2, family, na.action, weights, offset, prior,
-                           controlGLMfit, keepY, keepX, iterate, tol, maxIt,
-                           nIt, verbose, ...)
-
-  out$call <- cl
-
-  class(out) <- c("walsGLMmatrix", "walsGLM", "walsMatrix", "wals")
-  return(out)
-}
-
-#' Fits a GLM with the Weighted-Average Least Squares method
-#'
-#' **WARNING:** Interactions in formula do work work properly yet.
-#' It is recommended to manually create the interactions beforehand and then
-#' to insert them as 'linear terms' in the formula.
-#'
-#' @rdname walsGLM
-#'
-#' @param formula an object of class "\link{Formula}"
-#' (or one that can be coerced to that class):
-#' a symbolic description of the model to be fitted.
-#' The details of model specification are given under ‘Details’.
-#' @param data an optional data frame, list or environment
-#' (or object coercible by as.data.frame to a data frame) containing the
-#' variables in the model. If not found in data, the variables are taken from
-#' environment(formula), typically the environment from which walsNB is called.
-#' @param subset an optional vector specifying a subset of observations to be
-#' used in the fitting process.
-#' @param weights **not implemented yet.**
-#' @param offset **not implemented yet.**
-#' @param na.action **not implemented yet.**
-#' @param iterate if TRUE then the WALS algorithm is iterated using the previous
-#' estimates as starting values
-#' @param tol Only used if iterate = TRUE and nIt = NULL. If the Euclidean distance
-#' between the previous beta and current beta falls below tol, then the algorithm stops.
-#' @param maxIt Only used it iterate = TRUE and nIt = NULL. Aborts iterative fitting
-#' when number of iterations exceed maxIt
-#' @param nIt Only used if iterate = TRUE. If this is specified, then tol is ignored
-#' and the algorithm iterates nIt times. This option should not be used unless
-#' the user has a specific reason to run the algorithm nIt times, e.g. for
-#' replication purposes.
-#' @param verbose If verbose = TRUE, then it prints the iteration process
-#' (only relevant if iterate = TRUE).
-#' @param ... Arguments for workhorse \link[WALS]{walsGLMfit}.
-#'
-#' @details
-#' Formulas should always contain two parts, i.e. they should be of the form
-#' "y ~ X11 + X12 | X21 + X22", where the variables before "|" are the focus
-#' regressors (includes a constant by default) and the ones after "|" are the
-#' auxiliary regressors.
-#'
-#' @examples
-#' data("HMDA", package = "AER")
-#' fitBinomial <- walsGLM(deny ~ pirat + hirat + lvrat + chist + mhist + phist |
-#'                        selfemp + afam, data = HMDA, family = binomialWALS(),
-#'                        prior = weibull())
-#' summary(fitBinomial)
-#'
-#' data("NMES1988", package = "AER")
-#' fitPoisson <- walsGLM(emergency ~ health + chronic + age + gender |
-#'                       I((age^2)/10) + married + region, data = NMES1988,
-#'                       family = poissonWALS(), prior = laplace())
-#' summary(fitPoisson)
-#'
-#' @export
-walsGLM.formula <- function(formula, family, data, subset = NULL,
-                            na.action = NULL, weights = NULL, offset = NULL,
-                            prior = weibull(), controlGLMfit = list(),
-                            model = TRUE, keepY = TRUE, keepX = FALSE,
-                            iterate = FALSE, tol = 1e-6, maxIt = 10000, nIt = NULL,
-                            verbose = FALSE, ...) {
-
-  ## call
-  cl <- match.call()
-  if (missing(data)) data <- environment(formula)
-  mf <- match.call(expand.dots = FALSE)
-  m <- match(c("formula", "data", "subset", "na.action", "weights", "offset"), names(mf), 0L)
-  mf <- mf[c(1L, m)]
-  mf$drop.unused.levels <- TRUE
-
-  ## formula
-  oformula <- as.formula(formula)
-  formula <- Formula::as.Formula(formula)
-
-  # remove intercept in 2nd part by default
-  # if 2nd part already includes "-1" it will remain unchanged
-  # formula <- update(formula, . ~ . | . + -1)
-
-  if (length(formula)[2L] < 2L) {
-    # TODO: Implement what happens when only one part is specified in formula
-    # Either interpret all as focus regressors (same as ML estimator basically)
-    # or include all as auxiliary regressors and keep only constant as
-    # focus regressor.
-
-    stop("One part formula not implemented yet")
-    # formula <- as.Formula(formula(formula), ~ 1)
-    # simpleFormula <- TRUE
-  } else {
-    if (length(formula)[2L] > 2L) {
-      formula <- Formula::Formula(formula(formula, rhs = 1:2))
-      warning("formula must not have more than two RHS parts")
-    }
-    simpleFormula <- FALSE
-  }
-  mf$formula <- formula
-
-  ## evaluate model.frame
-  mf[[1L]] <- as.name("model.frame")
-  mf <- eval(mf, parent.frame())
-
-  ## extract terms, model matrix, response
-  mm <- extractModel(formula, mf, data)
-
-  # extract objects from mm
-  Y <- mm$Y; X1 <- mm$X1; X2 <- mm$X2; mt <- mm$mt; mtX1 <- mm$mtX1; mtX2 <- mm$mtX2
-  cont <- mm$cont
-  n <- length(Y)
-
-  ## weights (not used yet)
-  weights <- processWeights(weights, mf, n)
-
-  ## offsets (not used yet)
-  offset <- getOffset(formula, mf, cl, n)
-
-  ## Fit model
-  out <- walsGLMfitIterate(Y, X1, X2, family, na.action, weights, offset, prior,
-                           controlGLMfit, keepY, keepX, iterate, tol, maxIt,
-                           nIt, verbose, ...)
-
-  # add more elements
-  out$call <- cl
-  out$formula <- oformula
-  out$terms <- list(focus = mtX1, aux = mtX2, full = mt)
-  out$levels <- list(focus = .getXlevels(mtX1, mf), aux = .getXlevels(mtX2, mf),
-                     full = .getXlevels(mt, mf))
-  out$contrasts <- cont
-  if (model) out$model <- mf
-
-
-  class(out) <- c("walsGLM", "wals")
   return(out)
 }
 
