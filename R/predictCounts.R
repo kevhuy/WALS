9e422502d8bc0f14da01a89e2b8ebe6b8989dfc0
kevhuy <kevin.huynh@unibas.ch>
2023-09-20 12:01:25 +0200

    Updated docs of predict.walsGLM() and predictCounts(): Added new reference and restructured docs of predict.walsGLM().

--- R/predictCounts.R
+++ R/predictCounts.R
@@ -33,7 +33,7 @@ predictCounts <- function(x, ...) UseMethod("predictCounts", x)
 #' @details
 #' The \code{predictCounts.familyWALScount()} method is a modified version of the
 #' \code{\link[countreg]{predict.hurdle}} method from the \code{countreg} package
-#' version 0.2-1 (2023-06-13) \insertCite{countreg}{WALS} using the argument
+#' version 0.2-1 (2023-06-13) \insertCite{countreg,countreghurdle}{WALS} using the argument
 #' \code{type = "prob"}.
 #'
 #' @returns Returns a matrix of dimension \code{length(eta)} times
